ad65c7088fe3daf3c76f47c8b4d5681fcd114c62
fidepus <mail@fidepus.de>
2012-01-29 20:22:23 +0100

    rename to Zettelkasten

---
+++ src/de/fidepus/Zettelkasten/Dreimal.java
@@ -0,0 +1,21 @@
+package de.fidepus.Zettelkasten;
+
+import de.fidepus.Zettelkasten.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Dreimal extends Activity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dummytext);
+        TextView textview = new TextView(this);
+        textview.setText("Hier ist später alles drei mal gelernte.");
+       // getWindow().addContentView(textview, new LayoutParams());
+         setContentView(textview);
+    	
+    }
+}
---
+++ src/de/fidepus/Zettelkasten/Zweimal.java
@@ -0,0 +1,21 @@
+package de.fidepus.Zettelkasten;
+
+import de.fidepus.Zettelkasten.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Zweimal extends Activity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dummytext);
+        TextView textview = new TextView(this);
+        textview.setText("Hier ist später alles zwei mal gelernte.");
+       // getWindow().addContentView(textview, new LayoutParams());
+         setContentView(textview);
+    	
+    }
+}
